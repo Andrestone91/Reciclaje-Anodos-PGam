8c5e669ef5250bf5bfda1dd575253324951b313b
Andrestone91 <andrestone9191@gmail.com>
2025-05-23 12:12:32 -0300

    0.3

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,8 +6,22 @@ function Home() {
   // icon end
   return (
     <div className={css.bodyc}>
-      <section>
-        <h1>home</h1>
+      <section id="bienvenida" className={css.bienvenida}>
+        <div className={css["bienvenida_contenedor"]}>
+          <div className={css["bienvenida__contenedor-text"]}>
+            <h1 className={css["titulo-principal"]}>Bienvenidos a&nbsp;</h1>
+             <h1 className={css["titulo-principal-animacion"]}>Reciclaje Anodos PGam</h1>
+          </div>
+        </div>
+      </section>
+      <section id="nosotros" className={css.nosotros}>
+        <h1>Nosotros</h1>
+      </section>
+      <section id="servicios" className={css.servicios}>
+        <h1>Servicios</h1>
+      </section>
+      <section id="contacto" className={css.contacto}>
+        <h1>Contacto</h1>
       </section>
     </div>
   );
